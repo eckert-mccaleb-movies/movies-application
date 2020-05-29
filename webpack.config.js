08fc2aaadb5708d38d10d20bbb041817856a07d7
Chris Eckert <chris.eckert08@gmail.com>
2020-05-29 14:54:29 -0500

    issues with npm bootstrap - uninstalled - using bootstrap CDN

--- webpack.config.js
+++ webpack.config.js
@@ -28,10 +28,6 @@ module.exports = {
             presets: ['env']
           }
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
       }
     ]
   },
